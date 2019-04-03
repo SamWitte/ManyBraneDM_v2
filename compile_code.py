c5e76fe2f87d5a4a6c650936ab49c4f36ed2a04a
Samuel Witte <witte.sam@gmail.com>
2019-04-03 12:42:22 +0200

    modified compiler to incldue mkdir

--- compile_code.py
+++ compile_code.py
@@ -1,2 +1,8 @@
 import os
+
+if not os.path.isdir('OutputFiles'):
+    os.mkdir('OutputFiles')
+if not os.path.isdir('precomputed'):
+    os.mkdir('precomputed')
+
 os.system('python setup.py build_ext --inplace')
